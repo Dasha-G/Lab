68d396539ff9f0e978465a066eccd0ccbaf72fe2
Дарья Гаврилюк <dashagavriluyk@gmail.com>
2024-11-13 09:12:36 +0300

    new ver

--- 2.1 lab ipz/2.1 lab ipz.cpp
+++ 2.1 lab ipz/2.1 lab ipz.cpp
@@ -1,78 +1,130 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <cmath>
+#include <limits>
+#include <string>
+#include <regex>
 
 using namespace std;
 
-int main() {
-    double a, b, step;
-    int n;
+bool isInteger(const string& str) {
+    for (char c : str) {
+        if (!isdigit(c)) return false;
+    }
+    return true;
+}
 
-    cout << "Enter the range start (a): ";
-    cin >> a;
-    cout << "Enter the range end (b): ";
-    cin >> b;
-    cout << "Enter the step value: ";
-    cin >> step;
-    cout << "Enter the value of n: ";
-    cin >> n;
 
-    ofstream out("resultforequation.txt", ios::app);
-    if (out.is_open()) {
-        if (n <= 7) {
-            cout << "Error. n value is less or equal than 7 " << endl;
-            out << "Error: n value is less or equal than 7 " << endl;
-            return 1;
+bool isValidDouble(const string& str) {
+    regex doubleRegex("^[+-]?([0-9]*[.])?[0-9]+$");
+    return regex_match(str, doubleRegex);
+}
+
+double calculate_y(double x, int n) {
+    double y;
+    if (x < 7) {
+        double product = 1.0;
+        for (int i = 3; i <= n - 4; i++) {
+            product *= x / (5 + i);
         }
-        else if (cin.fail()) {
-            cout << "Error. The entered data is not a number " << endl;
-            out << "Error: The entered data is not a number " << endl;
-            return 1;
+        y = product;
+    }
+    else {
+        double sum = 0.0;
+        for (int j = 0; j <= n; j++) {
+            double product = 1.0;
+            for (int i = 1; i <= n + 1; i++) {
+                product *= (1.0 / (2 * x) + (3 * i) / (i - j + 1));
+            }
+            sum += product;
         }
-        else if (step <= 0) {
-            cout << "Error: step must be more than 0" << endl;
-            out << "Error: step must be more than 0" << endl;
-            return 1;
+        y = sum;
+    }
+    return y;
+}
+
+double getValidDoubleInput(const string& prompt) {
+    string input;
+    while (true) {
+        cout << prompt;
+        cin >> input;
+        if (isValidDouble(input)) {
+            return stod(input);  
         }
         else {
-            int max_iterations = 1000;  
+            cout << "Invalid input. Please enter a valid number: ";
+        }
+    }
+}
+
+int main() {
+    double a, b, step;
+    int num;
+
+    while (true) {
+    start:
+        cout << "Start (enter 0 to stop or integer to start): ";
+        cin >> num;
+
+        if (num == 0) {
+            break;
+        }
+
+        
+        a = getValidDoubleInput("Enter the range start (a): ");
+
+    sstart:
+        
+        b = getValidDoubleInput("Enter the range end (b): ");
+        if (a > b) {
+            cout << "Error. a must be less than or equal to b" << endl;
+            goto sstart;
+        }
+
+    ssstart:
+        
+        step = getValidDoubleInput("Enter the step value: ");
+        if (step <= 0) {
+            cout << "Invalid input. Step must be greater than 0: ";
+            goto ssstart;
+        }
+
+        string n_str;
+    nv:
+        cout << "Enter the value of n: ";
+        cin >> n_str;
+        if (!isInteger(n_str) || stoi(n_str) <= 7) {
+            cout << "Error: n must be an integer greater than 7." << endl;
+            goto nv;
+        }
+
+        int n = stoi(n_str);
+
+        ofstream out("resultforequation.txt", ios::app);
+        if (out.is_open()) {
+            int max_iterations = 1000;
             int iteration_count = 0;
+
             for (double x = a; x <= b; x += step) {
                 iteration_count++;
                 if (iteration_count > max_iterations) {
                     cout << "Error: Maximum iteration count exceeded." << endl;
                     out << "Error: Maximum iteration count exceeded." << endl;
-                    break;  
+                    goto start;
                 }
-                double y;
-                if (x < 7) {
-                    double product = 1.0;
-                    for (int i = 3; i <= n - 4; i++) {
-                        product *= x / (5 + i);
-                    }
-                    y = product;
-                }
-                else {
-                    double sum = 0.0;
-                    for (int j = 0; j <= n; j++) {
-                        double product = 1.0;
-                        for (int i = 1; i <= n + 1; i++) {
-                            product *= (1.0 / (2 * x) + (3 * i) / (i - j + 1));  
-                        }
-                        sum += product;
-                    }
-                    y = sum;
-                }
+
+                double y = calculate_y(x, n);
                 cout << "For x = " << x << ", y = " << y << endl;
                 out << "For x = " << x << ", y = " << y << endl;
             }
-        }
 
-        out.close();
-        cout << "File has been written" << endl;
+            out.close();
+            cout << "Finished processing. Do you want to continue?" << endl;
+            goto start;
+        }
     }
+
     return 0;
 }
-
 
 
